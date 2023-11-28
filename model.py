40d6080f36398ab07032e2d2fcc0952395122782
shifanzhu <shifzhu@gmail.com>
2023-11-27 22:54:05 -0500

    minor modification

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import skimage.transform as trans
 import numpy as np
 from keras.models import *
 from keras.layers import *
+# from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
